553988d2b197182b8928aab6d47e04c05d4376f9
hyeonseok <snrnakat@naver.com>
2021-03-14 18:00:32 +0900

    modify

--- src/Fahrenheit.java
+++ src/Fahrenheit.java
@@ -2,16 +2,6 @@ import java.util.Scanner;
 
 public class Fahrenheit {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
 		
-		System.out.println("È­¾¾¿Âµµ ÀÔ·Â : ");
-	 
-		int fahrenheit; 
-	    fahrenheit = sc.nextInt();
-	    
-	    double celcius;
-	    celcius = (double)5/9 * (fahrenheit-32);
-	    
-	    System.out.printf("¼·¾¾: %f" , celcius);
 	}
 }
