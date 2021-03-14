3254660e87b2d3e2fb7fa6971242b13afeb2a3fe
hyeonseok <snrnakat@naver.com>
2021-03-14 17:23:42 +0900

    add source

---
+++ src/Assignment.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Assignment {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+	
+		String str = sc.nextLine();
+	
+		System.out.println(str);
+	
+	}
+}
