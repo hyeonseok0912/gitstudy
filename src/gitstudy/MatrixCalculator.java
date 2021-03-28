6129c4e62dd4eeb565e4852ca2973937936611ef
hyeonseok <snrnakat@naver.com>
2021-03-28 11:47:31 +0900

    made a matrixcalculator program

--- src/gitstudy/MatrixCalculator.java
+++ src/gitstudy/MatrixCalculator.java
@@ -3,8 +3,10 @@ package gitstudy;
 public class MatrixCalculator {
 
 	public static void main(String[] args) {
-		int[][] a = { { 1, 0, 2 }, { -1, 3, 1 } };
-		int[][] b = { { 3, 2, 1 }, { 1, 1, 0 } };
+		int[][] a = { { 1, 0, 2 },
+					{ -1, 3, 1 } };
+		int[][] b = { { 3, 2, 1 }, 
+					{ 1, 1, 0 } };
 		int[][] c = new int[2][2];
 
 		for (int i = 0; i < 2; i++) {
