a1d57c7bf374df1c8daf8d7cd452214f4941bbfb
hyeonseok <snrnakat@naver.com>
2021-06-08 14:29:17 +0900

    modified the indentation

--- src/TemperatureChangerGui/TemperatureChangerGui.java
+++ src/TemperatureChangerGui/TemperatureChangerGui.java
@@ -4,50 +4,50 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TemperatureChangerGui extends JFrame {
-   public TemperatureChangerGui() {
+	public TemperatureChangerGui() {
 
-      setSize(350, 200);
-      setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setLocationRelativeTo(null);
+		setSize(350, 200);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
 
-      setTitle("MyFrame");
+		setTitle("MyFrame");
 
-      JPanel panel1 = new JPanel();
-      JLabel label1 = new JLabel("Fahrenheit temperature", JLabel.LEFT);
-      JTextField cTemp = new JTextField(10);
+		JPanel panel1 = new JPanel();
+		JLabel label1 = new JLabel("Fahrenheit temperature", JLabel.LEFT);
+		JTextField cTemp = new JTextField(10);
 
-      JPanel panel2 = new JPanel();
-      JLabel label2 = new JLabel("Celsius temperature", JLabel.LEFT);
-      JTextField fTemp = new JTextField(10);
+		JPanel panel2 = new JPanel();
+		JLabel label2 = new JLabel("Celsius temperature", JLabel.LEFT);
+		JTextField fTemp = new JTextField(10);
 
-      JPanel panel3 = new JPanel();
-      JButton button = new JButton("Convert");
+		JPanel panel3 = new JPanel();
+		JButton button = new JButton("Convert");
 
-      panel1.add(label1);
-      panel1.add(cTemp);
+		panel1.add(label1);
+		panel1.add(cTemp);
 
-      panel2.add(label2);
-      panel2.add(fTemp);
+		panel2.add(label2);
+		panel2.add(fTemp);
 
-      panel3.add(button);
-      button.addActionListener(e -> {
-         if (cTemp.getText().isEmpty())
-            fTemp.setText("");
-         else {
-            String s = cTemp.getText();
-            double c = Double.parseDouble(s);
-            double f = c * 9.0 / 5.0 + 32;
-            fTemp.setText("" + f);
-         }
-      });
+		panel3.add(button);
+		button.addActionListener(e -> {
+			if (cTemp.getText().isEmpty())
+				fTemp.setText("");
+			else {
+				String s = cTemp.getText();
+				double c = Double.parseDouble(s);
+				double f = c * 9.0 / 5.0 + 32;
+				fTemp.setText("" + f);
+			}
+		});
 
-      add(panel1, BorderLayout.NORTH);
-      add(panel2, BorderLayout.CENTER);
-      add(panel3, BorderLayout.SOUTH);
-      setVisible(true);
-   }
+		add(panel1, BorderLayout.NORTH);
+		add(panel2, BorderLayout.CENTER);
+		add(panel3, BorderLayout.SOUTH);
+		setVisible(true);
+	}
 
-   public static void main(String[] args) {
-      new TemperatureChangerGui();
-   }
+	public static void main(String[] args) {
+		new TemperatureChangerGui();
+	}
 }
